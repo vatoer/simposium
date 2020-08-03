2b1d5ab8c37fe77650d0acd942dfc59a7cd70f89
vatoer <vatoer.ckplus@gmail.com>
2020-08-03 15:44:03 +0700

    senin sore

--- app/src/main/java/com/starganteknologi/simposium/ui/sales/SalesFragment.kt
+++ app/src/main/java/com/starganteknologi/simposium/ui/sales/SalesFragment.kt
@@ -1,4 +1,4 @@
-package com.starganteknologi.simposium.ui.home
+package com.starganteknologi.simposium.ui.sales
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,20 +10,20 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.starganteknologi.simposium.R
 
-class HomeFragment : Fragment() {
+class SalesFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    private lateinit var salesViewModel: SalesViewModel
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+        salesViewModel =
+                ViewModelProviders.of(this).get(SalesViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_sales, container, false)
+        val textView: TextView = root.findViewById(R.id.text_sales)
+        salesViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
--- app/src/main/java/com/starganteknologi/simposium/ui/sales/SalesViewModel.kt
+++ app/src/main/java/com/starganteknologi/simposium/ui/sales/SalesViewModel.kt
@@ -1,13 +1,13 @@
-package com.starganteknologi.simposium.ui.home
+package com.starganteknologi.simposium.ui.sales
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class SalesViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "This is sales Fragment"
     }
     val text: LiveData<String> = _text
 }
